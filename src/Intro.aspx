0d85f7147fa1a88276e8d022eadbce2d9717d7cc
Jing <moonlikestone@gmail.com>
2019-08-28 18:00:16 +0800

    處理好介面及部分功能
    差Create出來的Card的方法還沒寫

--- src/Intro.aspx
+++ src/Intro.aspx
@@ -7,18 +7,24 @@
             <br />
             <br />
             <br />
-            <br />
-            <br />
-            <br />
-            <br />
+
         </div>
         <div class="row">
             <div class="col-md-3">
-                <h4 id="username" runat="server"></h4>
+                <h4>帳戶資訊</h4>
                 <hr />
-                <br />
-                <p>報名狀況：</p>
-<%--                <p style="text-align: right;" id="earthquakeStute" runat="server">123</p>
+                <p>姓名：</p>
+                <p id="user_Name" style="color: darkgray" runat="server"></p>
+                <p>學校：</p>
+                <p id="user_School" style="color: darkgray" runat="server"></p>
+                <p>聯絡電話：</p>
+                <p id="user_Phone" style="color: darkgray" runat="server"></p>
+                <p>聯絡信箱：</p>
+                <p id="user_Email" style="color: darkgray" runat="server"></p>
+                <button type="button" class="btn btn-outline-secondary" width="100%" data-toggle="modal" data-target="#UpdateModal"> 
+                    更改帳戶資訊
+                </button>
+                <%--                <p style="text-align: right;" id="earthquakeStute" runat="server">123</p>
                 <% if (Session["Login"].ToString() == "Y")
                     {%>
                 <p>前往報名</p>
@@ -29,22 +35,99 @@
                 <%  }%>--%>
             </div>
             <div class="col-md-9">
-                <h2>本次賽程
-                </h2>
+                <h4>參賽隊伍
+                </h4>
                 <hr />
-                <br />
+                <div class="card text-center">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                            <li class="nav-item" id="card_">
+                                <a id="a_Earthquake" class="nav-link active" href="#" style="color: dimgray" runat="server" onserverclick="a_Earthquake_Click">團隊來對震</a>
+                            </li>
+                            <li class="nav-item">
+                                <a id="a_Bridge" class="nav-link" href="#" style="color: dimgray" runat="server" onserverclick="a_Bridge_Click">橋梁變變變</a>
+                            </li>
+                            <li class="nav-item">
+                                <a id="a_Film" class="nav-link" href="#" tabindex="-1" aria-disabled="true" style="color: dimgray" runat="server" onserverclick="a_Film_Click">影領創視界</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <asp:Button ID="btn_NewTeam" runat="server" CssClass="btn btn-outline-secondary float-right" Style="margin-bottom: 15px;" Text="新增隊伍" OnClick="btn_NewTeam_Click" />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div id="div_TeamInfo" runat="server"></div>
+                                <%--      Cardtemplate            <div class="card">
+                                    <div class="card-body" style="text-align: left;">
+                                        <div class="row">
+                                            <div class="col-7">
+                                                <h6 style="margin-top: 5px;">隊伍名稱</h6>
+                                            </div>
+                                            <div class="col-5 ">
+
+                                                <a id="edit_team1" class="btn btn-outline-secondary float-right" style="height: 32px; font-size: 12px; width: 70px;">
+                                                    <img width="15px" style="margin-bottom: 4px;" src="https://img.icons8.com/ios-glyphs/64/000000/edit.png">
+                                                    <span>Edit</span>
+                                                </a>
+                                                <a id="view_team1" class="btn btn-outline-secondary float-right" style="height: 32px; font-size: 12px; margin-right: 5px; width: 70px;">
+                                                    <img width="15px" style="margin-bottom: 4px;" src="https://img.icons8.com/ios-glyphs/24/000000/visible.png">
+                                                    <span>View</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>--%>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
             <br />
             <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
         </div>
+    </div>
+    <div class="modal fade" id="UpdateModal" tabindex="-1" role="dialog" aria-labelledby="UpdateModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">變更帳戶資訊</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group row">
+                            <div class="col-sm-1"></div>
+                            <label for="updatePhone" class="col-sm-3 col-form-label float-left">連絡電話：</label>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control  float-left" id="updatePhone" runat="server">
+                            </div>
+                            <div class="col-sm-1"></div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-1"></div>
+                            <label for="updateEmail" class="col-sm-3 col-form-label">聯絡信箱：</label>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control" id="updateEmail" runat="server">
+                            </div>
+                            <div class="col-sm-1"></div>
+                        </div>
 
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-info" data-dismiss="modal" id="btn_updateAccount" runat="server" onserverclick="btn_updateAccount_ServerClick">儲存變更</button>
+                </div>
+            </div>
+        </div>
     </div>
 </asp:Content>
+
+
--- src/Intro.aspx.cs
+++ src/Intro.aspx.cs
@@ -6,12 +6,12 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace SignUpSystem
 {
     public partial class Intro : System.Web.UI.Page
     {
-        SqlConnection conn;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["Login"] == null)
@@ -19,8 +19,155 @@ namespace SignUpSystem
             if (Session["LoginId"] == null)
                 Session["LoginId"] = "1";
 
+            //讀抗震大作戰的隊伍資訊
+            if (!IsPostBack)
+            {
+                LoadTeamByAccount(TeamType.Earthquake);
+                LoadAccountInfo();
+            }
+        }
+        protected void a_Earthquake_Click(object sender, EventArgs e)
+        {
+            RemoveActive();
+            a_Earthquake.Attributes["class"] += " active";
+            LoadTeamByAccount(TeamType.Earthquake);
+        }
+        protected void a_Bridge_Click(object sender, EventArgs e)
+        {
+            RemoveActive();
+            a_Bridge.Attributes["class"] += " active";
+            LoadTeamByAccount(TeamType.Bridge);
+        }
+        protected void a_Film_Click(object sender, EventArgs e)
+        {
+            RemoveActive();
+            a_Film.Attributes["class"] += " active";
+            LoadTeamByAccount(TeamType.Film);
+        }
+        private void LoadTeamByAccount(TeamType type)
+        {
+            //Loading Team Function
+            //Connect to SQL DB
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlDB"].ConnectionString);
+            conn.Open();
+            SqlCommand command = new SqlCommand();
 
+            switch (type)
+            {
+                case TeamType.Earthquake:
+                    command = new SqlCommand($"SELECT Id, Name FROM EarthquakeTeam WHERE AccountID = {Session["LoginId"]} AND (CreateDate BETWEEN '2019-01-01 00:00:00.000' AND '2019-12-31 00:00:00.000')", conn);
+                    break;
+                case TeamType.Bridge:
+                    command = new SqlCommand($"SELECT Id, Name FROM BridgeTeam WHERE AccountID = {Session["LoginId"]} AND (CreateDate BETWEEN '2019-01-01 00:00:00.000' AND '2019-12-31 00:00:00.000')", conn);
+                    break;
+                case TeamType.Film:
+                    command = new SqlCommand($"SELECT Id, Name FROM FilmInfo WHERE AccountID = {Session["LoginId"]} AND (CreateDate BETWEEN '2019-01-01 00:00:00.000' AND '2019-12-31 00:00:00.000')", conn);
+                    break;
+            }
+
+            SqlDataReader dataReader = command.ExecuteReader();
+            string innerHtmlStr = "";
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    IDataRecord record = (IDataRecord)dataReader;
+                    string teamName = record["Name"].ToString();
+                    string teamId = record["Id"].ToString();
+                    innerHtmlStr += "<div class=\"card\">"
+                        + "<div class=\"card-body\" style=\"text-align: left; \">"
+                        + "<div class=\"row\">"
+                        + "<div class=\"col-7\">"
+                        + "<h6 style=\"margin-top: 5px;\">" + teamName + "</h6>"
+                        + "</div><div class=\"col-5 \">"
+                        + "<a id=\"" + teamId + "\" class=\"btn btn-outline-secondary float-right\" style=\"height: 32px; font-size: 12px; width: 70px;\">"
+                        + "<img width=\"15px\" style=\"margin-bottom: 4px;\" src=\"https://img.icons8.com/ios-glyphs/64/000000/edit.png \">"
+                        + "<span>Edit</span>"
+                        + "</a>"
+                        + "<a id=\"view_team1\" class=\"btn btn-outline-secondary float-right\" style=\"height: 32px; font-size: 12px; margin-right: 5px; width: 70px;\">"
+                        + "<img width=\"15px\" style=\"margin-bottom: 4px;\" src=\"https://img.icons8.com/ios-glyphs/24/000000/visible.png \">"
+                        + "<span>View</span>"
+                        + "</a>"
+                        + "</div></div></div></div>";
+                }
+            }
+            else
+            {
+                innerHtmlStr += "<div class=\"card\">"
+                    + "<div class=\"card-body\" style=\"color: dimgray; text-align: center; \">"
+                    + "<div class=\"row\">"
+                    + "<div class=\"col-12\" style=\"margin-top: 5px; \">"
+                    + "<h6>尚未開始報名</h6>"
+                    + "</div></div></div></div>";
+            }
+            div_TeamInfo.InnerHtml = innerHtmlStr;
+            conn.Close();
+        }
+        private void RemoveActive()
+        {
+            a_Earthquake.Attributes["class"] = "nav-link";
+            a_Bridge.Attributes["class"] = "nav-link";
+            a_Film.Attributes["class"] = "nav-link";
+        }
+        protected void btn_NewTeam_Click(object sender, EventArgs e)
+        {
+            if (a_Earthquake.Attributes["class"].Contains("active"))
+            {
+                //去填抗震盃報名資料
+            }
+            else if (a_Bridge.Attributes["class"].Contains("active"))
+            {
+                //去填橋梁變變變資料
+            }
+            else
+            {
+                //去填微電影資料
+            }
+        }
+        private void LoadAccountInfo()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlDB"].ConnectionString);
+            conn.Open();
+            SqlCommand command = new SqlCommand($"SELECT * FROM Account WHERE Id = {Session["LoginId"]}", conn);
+            SqlDataReader reader = command.ExecuteReader();
+            int schoolid = 0;
+            while (reader.Read())
+            {
+                IDataRecord record = (IDataRecord)reader;
+                user_Name.InnerText = record["Name"].ToString();
+                user_Phone.InnerText = record["Phone"].ToString();
+                updatePhone.Value = record["Phone"].ToString();
+                user_Email.InnerText = record["Email"].ToString();
+                updateEmail.Value = record["Email"].ToString();
+                schoolid = Convert.ToInt32(record["SchoolID"]);
+            }
+            reader.Close();
+            command = new SqlCommand($"SELECT Name FROM School WHERE Id = {schoolid}", conn);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                IDataRecord record = (IDataRecord)reader;
+                user_School.InnerText = record["Name"].ToString();
+            }
+
+            conn.Close();
         }
 
+        protected void btn_updateAccount_ServerClick(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlDB"].ConnectionString);
+            conn.Open();
+            SqlCommand command = new SqlCommand($"UPDATE Account SET Phone = '{updatePhone.Value.ToString()}', Email = '{updateEmail.Value.ToString()}' WHERE Id = {Session["LoginId"]}", conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+
+            LoadAccountInfo();
+        }
+    }
+    enum TeamType
+    {
+        Earthquake,
+        Bridge,
+        Film
     }
 }
