3712cc03571542778d17afe4c9988df92d6c60a2
Jing <moonlikestone@gmail.com>
2019-09-04 17:48:43 +0800

    Update C#
    Finish

--- src/FilmRegistration.aspx.designer.cs
+++ src/FilmRegistration.aspx.designer.cs
@@ -11,5 +11,59 @@ namespace SignUpSystem {
     
     
     public partial class FilmRegistration {
+        
+        /// <summary>
+        /// select_Team 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect select_Team;
+        
+        /// <summary>
+        /// text_Design 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea text_Design;
+        
+        /// <summary>
+        /// text_Outline 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea text_Outline;
+        
+        /// <summary>
+        /// input_Link 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText input_Link;
+        
+        /// <summary>
+        /// btn_Submit 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btn_Submit;
+        
+        /// <summary>
+        /// Modal_Body 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Modal_Body;
     }
 }
